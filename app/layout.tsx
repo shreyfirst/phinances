536143a946fd5ca216e714700837f0c2df2cafa1
Shrey Gupta <shreysgupta@gmail.com>
2023-12-29 13:50:25 -0800

    metadata icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,10 @@ const defaultUrl = process.env.VERCEL_URL
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
   title: 'Phinances Portal',
-  description: 'Billing & reimbursements for Phi Delta Theta'
+  description: 'Billing & reimbursements for Phi Delta Theta',
+  icons: {
+    icon: '/icon.png'
+  },
 }
 
 export default function RootLayout({
